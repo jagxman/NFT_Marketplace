b2402a55ba827a22693fa61c33710d52fe4ac15c
JagMan177 <jagjitmangar177@gmail.com>
2023-07-20 00:40:26 -0400

    Listed NFT Completed.

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useContext } from 'react';
+import { NFTContext } from '../context/NFTContext';
+import { NFTCard, Loader, Banner } from '../components';
+import images from '../assets';
 
-const myNFTs = () => (
-  <div>
-    My NFTs.
-  </div>
-);
+const myNFTs = () => {
+  const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(NFTContext);
+  const [nfts, setNfts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div>
+      My NFTs.
+    </div>
+  );
+};
 
 export default myNFTs;
