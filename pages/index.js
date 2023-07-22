2e3999210ce52bb5be6d7f933f5280cd8a6030d9
JagMan177 <jagjitmangar177@gmail.com>
2023-07-22 02:04:16 -0400

    Homepage Fix -> Edge case of no listings.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef, useContext } from 'react';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { Banner, CreatorCard, NFTCard, SearchBar } from '../components';
+import { Banner, CreatorCard, NFTCard, SearchBar, Loader } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 import { getCreators } from '../utils/getTopCreators';
@@ -18,14 +18,14 @@ const Home = () => {
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
   const { theme } = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchNFTs()
       .then((items) => {
         setnfts(items);
         setnftsCopy(items);
-
-        console.log(items);
+        setIsLoading(false);
       });
   }, []);
 
@@ -108,31 +108,35 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          banner="Discover, collect, and sell extraordinary NFTs"
+          banner={(<>Discover, collect, and sell <br />extraordinary NFTs</>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyles="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
 
-        <div>
-          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2x1 minlg:text-4xl font-semibold ml-4 xs:ml-0">
-            Best Creators
-          </h1>
+        {!isLoading && !nfts.length ? (
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 sm:ml-0">That&apos;s weird... no NFTs for sale.</h1>
+        ) : isLoading ? <Loader /> : (
+          <>
+            <div>
+              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2x1 minlg:text-4xl font-semibold ml-4 xs:ml-0">
+                Top Sellers
+              </h1>
 
-          <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
-            <div
-              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
-              ref={scrollRef}
-            >
-              {topCreators.map((creator, i) => (
-                <CreatorCard
-                  key={creator.seller}
-                  rank={i + 1}
-                  creatorImage={images[`creator${i + 1}`]}
-                  creatorEths={creator.sum}
-                  creatorName={shortenAddress(creator.seller)}
-                />
-              ))}
-              {/* {[6, 7, 8, 9, 10].map((i) => (
+              <div className="relative flex-1 max-w-full flex mt-3" ref={parentRef}>
+                <div
+                  className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+                  ref={scrollRef}
+                >
+                  {topCreators.map((creator, i) => (
+                    <CreatorCard
+                      key={creator.seller}
+                      rank={i + 1}
+                      creatorImage={images[`creator${i + 1}`]}
+                      creatorEths={creator.sum}
+                      creatorName={shortenAddress(creator.seller)}
+                    />
+                  ))}
+                  {/* {[6, 7, 8, 9, 10].map((i) => (
                 <CreatorCard
                   key={`creator-${i}`}
                   rank={i}
@@ -141,56 +145,56 @@ const Home = () => {
                   creatorName={`0x${makeId(3)}...${makeId(4)}`}
                 />
               ))} */}
-              {!hideButtons && (
-                <>
-                  <div
-                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
-                    onClick={() => handleScroll('left')}
-                  >
-                    <Image
-                      src={images.left}
-                      layout="fill"
-                      objectFit="contain"
-                      alt="left"
-                      className={theme === 'light' && 'filter invert'}
-                    />
-                  </div>
+                  {!hideButtons && (
+                  <>
+                    <div
+                      className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                      onClick={() => handleScroll('left')}
+                    >
+                      <Image
+                        src={images.left}
+                        layout="fill"
+                        objectFit="contain"
+                        alt="left"
+                        className={theme === 'light' && 'filter invert'}
+                      />
+                    </div>
 
-                  <div
-                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
-                    onClick={() => handleScroll('right')}
-                  >
-                    <Image
-                      src={images.right}
-                      layout="fill"
-                      objectFit="contain"
-                      alt="left"
-                      className={theme === 'light' && 'filter invert'}
-                    />
-                  </div>
-                </>
-              )}
+                    <div
+                      className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                      onClick={() => handleScroll('right')}
+                    >
+                      <Image
+                        src={images.right}
+                        layout="fill"
+                        objectFit="contain"
+                        alt="left"
+                        className={theme === 'light' && 'filter invert'}
+                      />
+                    </div>
+                  </>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="mt-10">
-          <div className="flexBetween mx-4 xs:mx0 minlg:mx-8 sm:flex-col sm:items-start">
-            <h1 className="font-poppins dark:text-white text-nft-black-1 text-2x1 minlg:text-4xl font-semibold sm:mb-4 flex-1">
-              Top NFTs
-            </h1>
-            <div className="flex-2 w-full flex flex-row sm:flex-col">
-              <SearchBar
-                activeSelect={activeSelect}
-                setActiveSelect={setActiveSelect}
-                handleSearch={onHandleSearch}
-                clearSearch={onClearSearch}
-              />
-            </div>
-          </div>
-          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
-            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
-            {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+            <div className="mt-10">
+              <div className="flexBetween mx-4 xs:mx0 minlg:mx-8 sm:flex-col sm:items-start">
+                <h1 className="font-poppins dark:text-white text-nft-black-1 text-2x1 minlg:text-4xl font-semibold sm:mb-4 flex-1">
+                  Top NFTs
+                </h1>
+                <div className="flex-2 w-full flex flex-row sm:flex-col">
+                  <SearchBar
+                    activeSelect={activeSelect}
+                    setActiveSelect={setActiveSelect}
+                    handleSearch={onHandleSearch}
+                    clearSearch={onClearSearch}
+                  />
+                </div>
+              </div>
+              <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+                {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
               <NFTCard
                 key={`nft-${i}`}
                 nft={{
@@ -204,8 +208,13 @@ const Home = () => {
                 }}
               />
             ))} */}
-          </div>
-        </div>
+              </div>
+            </div>
+
+          </>
+
+        )}
+
       </div>
     </div>
   );
